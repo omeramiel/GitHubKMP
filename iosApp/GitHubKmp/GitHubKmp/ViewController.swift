1b0d55139b1964800b65aa618f12ba9ac76a61eb
Omer Amiel <omeramiel@gmail.com>
2019-10-22 17:30:16 +0300

    ios app updated

--- iosApp/GitHubKmp/GitHubKmp/ViewController.swift
+++ iosApp/GitHubKmp/GitHubKmp/ViewController.swift
@@ -1,74 +1,76 @@
-//
-//  ViewController.swift
-//  GitHubKmp
-//
-//  Created by Admin on 15/10/2019.
-//  Copyright © 2019 Admin. All rights reserved.
-//
-
 import UIKit
 import shared
 
 class ViewController: UIViewController, MembersView {
-    
-    @IBOutlet weak var greeting: UILabel!
-    
-    @IBOutlet weak var membersTableView: UITableView!
-    
-    let memberList =  MemberList()
-    
-    lazy var presenter: MembersPresenter = {
-        MembersPresenter(view: self, repository: AppDelegate.appDelegate.dataRepository, settings: AppDelegate.appDelegate.settings)
-    }()
-    
-    var isUpdating = false
+  
+  let memberList = MemberList()
+  
+  lazy var presenter: MembersPresenter = {
+    MembersPresenter(view: self, repository: AppDelegate.appDelegate.dataRepository, settings: AppDelegate.appDelegate.settings)
+  }()
+  
+  var isUpdating = false
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        greeting.text = Greeting().greeting()
-        membersTableView.register(UINib(nibName: "MemberCellTableViewCell", bundle: nil), forCellReuseIdentifier: "MemberCell")
-    }
+  @IBOutlet weak var greeting: UILabel!
+  @IBOutlet weak var membersTableView: UITableView!
+  @IBOutlet weak var organization: UITextField!
     
-    override func viewWillAppear(_ animated: Bool) {
-        presenter.onCreate()
-    }
+  override func viewWillAppear(_ animated: Bool) {
+    presenter.onCreate()
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    presenter.onDestroy()
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
     
-    override func viewWillDisappear(_ animated: Bool) {
-        presenter.onDestroy()
+    greeting.text =  Greeting().greeting()
+    
+    membersTableView.register(UINib(nibName: "MemberCellTableViewCell", bundle: nil), forCellReuseIdentifier: "MemberCell")
+  }
+  
+    @IBAction func show(_ sender: UIButton) {
+        if let organization = self.organization.text {
+          self.organization.resignFirstResponder()
+          presenter.update(organization: organization)
+        }
     }
 
-    func onUpdate(members: [Member], organization: String) {
-        memberList.updateMembers(newMembers: members)
-        membersTableView.reloadData()
-    }
+  func onUpdate(members: [Member], organization: String) {
+    self.organization.text = organization
+    self.memberList.updateMembers(newMembers: members)
+    self.membersTableView.reloadData()
+  }
 }
 
 extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.memberList.members.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MemberCell", for: indexPath) as! MemberCellTableViewCell
-        let member = self.memberList.members[indexPath.row]
-        cell.memberName.text = member.login
-        cell.memberAvatar.load(url: URL(string: member.avatar_url)!)
-        return cell
-    }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.memberList.members.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "MemberCell", for: indexPath) as! MemberCellTableViewCell
+    let member = self.memberList.members[indexPath.row]
+    cell.memberName.text = member.login
+    cell.memberAvatar.load(url: URL(string: member.avatar_url)!)
+    return cell
+  }
 }
 
 extension UIImageView {
-    func load(url: URL) {
-        DispatchQueue.global().async {
-            [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        self?.image = image
-                    }
-                }
-            }
+  func load(url: URL) {
+    DispatchQueue.global().async { [weak self] in
+      if let data = try? Data(contentsOf: url) {
+        if let image = UIImage(data: data) {
+          DispatchQueue.main.async {
+            self?.image = image
+          }
         }
+      }
     }
+  }
 }
-
